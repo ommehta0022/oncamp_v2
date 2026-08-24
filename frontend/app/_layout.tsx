87ff9216532ecc1ec0cfb7a8c8754d90ccedba51
ommehta0022 <ommehtaofficial@gmail.com>
2026-08-24 15:26:23 +0530

    fix(android): restore accessibility provider before theme startup

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -10,6 +10,7 @@ import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 
 import { useIconFonts } from "@/src/hooks/use-icon-fonts";
 import { ThemeProvider, useTheme } from "@/src/theme/ThemeProvider";
+import { AccessibilityProvider } from "@/src/context/AccessibilityProvider";
 import { RoleProvider } from "@/src/context/RoleProvider";
 import { LanguageProvider } from "@/src/context/LanguageProvider";
 import { NotificationProvider } from "@/src/context/NotificationProvider";
@@ -91,24 +92,26 @@ export default function RootLayout() {
       <GestureHandlerRootView style={{ flex: 1, width: "100%", backgroundColor: STARTUP_BACKGROUND }}>
         <SafeAreaProvider>
           <ActionSheetProvider>
-            <ThemeProvider>
-              <LanguageProvider>
-                <RoleProvider>
-                  <FeatureFlagsProvider>
-                    <NotificationProvider>
-                      <PushNotificationProvider>
-                        <ToastProvider>
-                          <ThemedStack />
-                          <AppUpdateGate />
-                          <ServerUpdateCoordinator />
-                          <SessionExpiredModal />
-                        </ToastProvider>
-                      </PushNotificationProvider>
-                    </NotificationProvider>
-                  </FeatureFlagsProvider>
-                </RoleProvider>
-              </LanguageProvider>
-            </ThemeProvider>
+            <AccessibilityProvider>
+              <ThemeProvider>
+                <LanguageProvider>
+                  <RoleProvider>
+                    <FeatureFlagsProvider>
+                      <NotificationProvider>
+                        <PushNotificationProvider>
+                          <ToastProvider>
+                            <ThemedStack />
+                            <AppUpdateGate />
+                            <ServerUpdateCoordinator />
+                            <SessionExpiredModal />
+                          </ToastProvider>
+                        </PushNotificationProvider>
+                      </NotificationProvider>
+                    </FeatureFlagsProvider>
+                  </RoleProvider>
+                </LanguageProvider>
+              </ThemeProvider>
+            </AccessibilityProvider>
           </ActionSheetProvider>
         </SafeAreaProvider>
       </GestureHandlerRootView>
